04da13ddffbc5bae7ef3a4535d645729e5b496e7
sergio lor <sergiolorenzoamarelle9@gmail.com>
2024-02-29 11:06:52 +0000

    orders_table_update

---
+++ models/example/orders.sql
@@ -0,0 +1,6 @@
+With orders AS (
+   SELECT O_ORDERKEY, O_CUSTKEY, O_ORDERSTATUS, O_TOTALPRICE,
+      O_ORDERDATE, O_ORDERPRIORITY, O_CLERK, O_SHIPPRIORITY
+   FROM tpch.orders
+)
+Select * from orders
