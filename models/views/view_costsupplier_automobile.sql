8ff8009135f6c7e3e020e54e7684b69accdc5ffd
sergio lor <sergiolorenzoamarelle9@gmail.com>
2024-03-05 12:15:10 +0000

    VIEW_COSTSUPPLIER_AUTOMOBILE_UPDATE

--- models/views/view_costsupplier_automobile.sql
+++ models/views/view_costsupplier_automobile.sql
@@ -1,5 +1,5 @@
 {{ config(materialized = 'view') }}
-
+-- Promedio del costo de suministro del proveedor para las partes de tipo AUTOMOBILE
 SELECT
     fo.O_ORDERKEY,
     fo.O_ORDERDATE,
@@ -19,12 +19,10 @@ SELECT
     p.P_SIZE AS PART_SIZE,
     p.P_CONTAINER AS PART_CONTAINER,
     ps.PS_SUPPLYCOST,
-    --calcular el total de ventas por cliente
-    SUM(fo.ORDER_COUNT * (p.P_RETAILPRICE - fo.ITEM_DISCOUNTED_AMOUNT)) OVER (PARTITION BY c.C_CUSTKEY) AS total_sales_per_customer,
-    -- Calcular el promedio de cantidad del impuesto por orden
-    AVG(fo.ITEM_TAX_AMOUNT) OVER () AS avg_tax_amount_per_order,
-    -- Clasificar las órdenes por fecha
-    RANK() OVER (ORDER BY fo.O_ORDERDATE) AS order_date_rank
+    -- Calcular el total de ventas por artículo
+    SUM(fo.ORDER_COUNT * (p.P_RETAILPRICE - fo.ITEM_DISCOUNTED_AMOUNT)) OVER (PARTITION BY p.P_PARTKEY) AS total_sales_per_part,
+    -- Calcular el promedio del costo de suministro por proveedor
+    AVG(ps.PS_SUPPLYCOST) OVER (PARTITION BY s.S_SUPPKEY) AS avg_supply_cost_per_supplier
 FROM
     FACT_ORDERS_LINE fo
 JOIN
@@ -35,7 +33,11 @@ JOIN
     PART p ON li.L_PARTKEY = p.P_PARTKEY
 JOIN
     PARTSUPP ps ON p.P_PARTKEY = ps.PS_PARTKEY
+JOIN
+    SUPPLIER s ON ps.PS_SUPPKEY = s.S_SUPPKEY
 WHERE
-    fo.O_ORDERSTATUS = 'F'  -- Filtrar solo las órdenes Finalizadas
+    fo.O_ORDERSTATUS = 'F'  -- Filtrar solo las órdenes enviadas
+    AND fo.O_ORDERDATE >= DATEADD(DAY, -7, CURRENT_DATE())  -- Filtrar órdenes de la última semana
+    AND c.C_MKTSEGMENT = 'AUTOMOBILE'  -- Filtrar clientes del segmento de mercado de automóviles
 ORDER BY
     fo.O_ORDERDATE
